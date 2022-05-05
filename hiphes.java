cca67ca1b2763407783d5dd2e04c0a7b1e96121a
hatice demirtas <98237665+hdmirts@users.noreply.github.com>
2022-05-05 17:19:07 +0300

    Create hiphes.java

---
+++ hiphes.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class hiphes {
+    public static void main (String [] args) {
+        double x, y, z;
+        Scanner giris = new Scanner (System.in);
+        System.out.print("1. Kenarı Girin :");
+        x = giris.nextInt();
+        System.out.print("2. Kenarı Girin :");
+        y = giris.nextInt();
+        
+        z = Math.sqrt ((x*x) + (y*y));
+        System.out.println ("Hipotenüs :" + z);
+
+
+    }
+}
